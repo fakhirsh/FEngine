e04c7e486705f90510499e61a60c466d2dee3258
Fakhir Shaheen <fakhir.shaheen@gmail.com>
2015-04-01 20:47:46 +0500

    Very simple shearing (hack) implemented.

---
+++ FEngine/PhysicsManager/GLES2DebugDraw.cpp
@@ -0,0 +1,9 @@
+//
+//  GLES2DebugDraw.cpp
+//  FEngine
+//
+//  Created by Fakhir Shaheen on 01/04/2015.
+//  Copyright (c) 2015 Fakhir Shaheen. All rights reserved.
+//
+
+#include "GLES2DebugDraw.h"
---
+++ FEngine/PhysicsManager/GLES2DebugDraw.h
@@ -0,0 +1,14 @@
+//
+//  GLES2DebugDraw.h
+//  FEngine
+//
+//  Created by Fakhir Shaheen on 01/04/2015.
+//  Copyright (c) 2015 Fakhir Shaheen. All rights reserved.
+//
+
+#ifndef __FEngine__GLES2DebugDraw__
+#define __FEngine__GLES2DebugDraw__
+
+#include <stdio.h>
+
+#endif /* defined(__FEngine__GLES2DebugDraw__) */
