02903ead7373ae9ca13358fbefbb57aa75ebf1da
Fakhir Shaheen <fakhir.shaheen@gmail.com>
2015-04-23 04:27:26 +0500

    FEngine::String PrependZeros function added

--- FEngine/Utility/String.cpp
+++ FEngine/Utility/String.cpp
@@ -13,6 +13,15 @@ namespace FEngine
     namespace String
     {
         
+        void PrependZeros(std::string & str, int totalDigits)
+        {
+            int zerosToPrepend = totalDigits - str.length();
+            
+            for(int i = 0; i < zerosToPrepend; i++)
+            {
+                str = std::string("0") + str;
+            }
+        }
         
     }
 }
--- FEngine/Utility/String.h
+++ FEngine/Utility/String.h
@@ -16,6 +16,12 @@ namespace FEngine
     namespace String
     {
         
+        void PrependZeros(std::string & str, int totalDigits);
+        
+        
+        // Why didn't I just declate the following function here and define it in another CPP file?
+        //    Because I don't know how to do it, it has comples function signature... :D
+        
         template < class T >
         std::string ToString(const T & number)
         {
@@ -23,6 +29,6 @@ namespace FEngine
             stm << number ;
             return stm.str() ;
         }
-
+        
     }
 }
